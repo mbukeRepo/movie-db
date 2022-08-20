db2ae911f0429b45a99d2136ebd39710a8c32f61
mbukeRepo <mbuke404@gmail.com>
2022-08-20 16:05:41 +0200

    reconfigured my navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LiveTvTwoToneIcon from '@mui/icons-material/LiveTvTwoTone';
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 import VideocamTwoToneIcon from '@mui/icons-material/VideocamTwoTone';
+import {Link} from "react-router-dom";
 import "./Navigation.scss";
 
 export default function Navigation() {
@@ -9,9 +10,21 @@ export default function Navigation() {
     <div className='main-navigation'>
         <p className='main-navigation-logo'>MovieDB</p>
         <ul className="main-navigation-list">
-            <li className="main-navigation__list-item"> <VideocamTwoToneIcon/> <span> Movies</span> </li>
-            <li className="main-navigation__list-item"> <LiveTvTwoToneIcon/> <span> Tv Shows</span></li>
-            <li className="main-navigation__list-item"> <SearchTwoToneIcon/> <span> Search</span> </li>
+            <li className="main-navigation__list-item"> 
+              <Link to={"/movies"}> 
+                <VideocamTwoToneIcon/> <span> Movies</span> 
+              </Link>
+            </li>
+            <li className="main-navigation__list-item"> 
+              <Link to={"/tv-shows"}>
+                <LiveTvTwoToneIcon/> <span> Tv Shows</span>  
+              </Link>
+            </li>
+            <li className="main-navigation__list-item"> 
+              <Link to={"/search"}>
+                  <SearchTwoToneIcon/> <span> Search</span>
+              </Link>
+            </li>
         </ul>
     </div>
   )
