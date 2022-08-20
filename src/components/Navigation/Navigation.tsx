02e16a06d9bd4f147e0718dd4cf2d3dbcb5f5654
mbukeRepo <mbuke404@gmail.com>
2022-08-20 15:34:05 +0200

    configured my navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import LiveTvTwoToneIcon from '@mui/icons-material/LiveTvTwoTone';
-import LocalMoviesTwoToneIcon from '@mui/icons-material/LocalMoviesTwoTone';
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
+import VideocamTwoToneIcon from '@mui/icons-material/VideocamTwoTone';
 import "./Navigation.scss";
 
 export default function Navigation() {
   return (
     <div className='main-navigation'>
         <p className='main-navigation-logo'>MovieDB</p>
-        <ul className="main-navigation__list">
-            <li className="main-navigation__list-item"> <LocalMoviesTwoToneIcon/> Movies</li>
-            <li className="main-navigation__list-item"> <LiveTvTwoToneIcon/> Tv Shows</li>
-            <li className="main-navigation__list-item"> <SearchTwoToneIcon/>Search</li>
+        <ul className="main-navigation-list">
+            <li className="main-navigation__list-item"> <VideocamTwoToneIcon/> <span> Movies</span> </li>
+            <li className="main-navigation__list-item"> <LiveTvTwoToneIcon/> <span> Tv Shows</span></li>
+            <li className="main-navigation__list-item"> <SearchTwoToneIcon/> <span> Search</span> </li>
         </ul>
     </div>
   )
