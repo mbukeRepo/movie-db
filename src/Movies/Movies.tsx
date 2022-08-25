020979f83425e99ca7dc288da86c16456ffd31a0
mbukeRepo <mbuke404@gmail.com>
2022-08-25 20:51:29 +0200

    modified the file to remove warning

--- src/Movies/Movies.tsx
+++ src/Movies/Movies.tsx
@@ -9,23 +9,35 @@ import List from "../components/List/List"
 
 const Movies = () => {
   const [popular, setPopular] = useState<Movie[]>([]);
+  const [topRated, setTopRated] = useState<Movie[]>([]);
   // const [topRated, setTopRated] = useState<Movie[]>();
   useEffect(() => {
     document.title = "Movies";
+    const popularURL = "https://api.themoviedb.org/3/movie/popular?api_key=67a92d3ba7cc55df830b73762d5501f0";
+    const topRatedURL = "https://api.themoviedb.org/3/movie/top_rated?api_key=67a92d3ba7cc55df830b73762d5501f0";
     let subscribed = true;
-    fetchPopular().then((movies: Movie[]) => {
+
+    // fetching popular movies
+    fetchMovies(popularURL).then((movies: Movie[]) => {
       if(subscribed){
         setPopular(prev => movies);
       }
     }).catch((err: Error) => console.log);
+
+    // fetching top rated
+    fetchMovies(topRatedURL).then((movies: Movie[]) => {
+      if(subscribed){
+        setTopRated(prev => movies);
+      }
+    }).catch((err: Error) => console.log);
+
     return () => {
       subscribed = false;
     }
   }, []);
-  const fetchPopular = async () => {
+  const fetchMovies = async (url: string) => {
     try {
-      const popularURL = "https://api.themoviedb.org/3/movie/popular?api_key=67a92d3ba7cc55df830b73762d5501f0"
-      const {data} = await axios.get(popularURL);
+      const {data} = await axios.get(url);
       console.log(data);
       const movies = data.results.map(({id, title, backdrop_path}: Movie) => ({title, id, backdrop_path}));
       return movies;  
@@ -36,10 +48,21 @@ const Movies = () => {
   }
   return (
     <div>
-      <div className="popular-list">
-        <p className="popular-list__title">Popular movies</p>
-        <List movies={popular}/>
-      </div>
+      {
+        (!popular && !topRated) ? 
+          <p>Loading</p>: 
+          <>
+             <div className="list">
+                <p className="list__title">Popular movies</p>
+                <List movies={popular}/>
+             </div>
+             <div className="list">
+                <p className="list__title">Top Rated movies</p>
+                <List movies={topRated}/>
+             </div>
+          </>
+      }
+      
     </div>
   )
 }
